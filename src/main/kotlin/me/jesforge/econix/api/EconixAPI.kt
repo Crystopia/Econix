8569b04e0c27ba5da76fc21814edbbedbe65381b
Jesper <jesper.l.richert@gmail.com>
2025-02-24 17:58:37 +0100

    Update EconixAPI.kt

--- src/main/kotlin/me/jesforge/econix/api/EconixAPI.kt
+++ src/main/kotlin/me/jesforge/econix/api/EconixAPI.kt
@@ -1,10 +1,14 @@
 ﻿package me.jesforge.econix.api
 
+import me.jesforge.econix.Main
 import me.jesforge.econix.functions.UserServices
 import org.bukkit.entity.Player
 
 class EconixAPI {
     companion object {
+
+        var econixPlugin = Main.instance
+
         fun getBalance(player: Player, currency: String): Double {
             return UserServices().getCurrency(player, currency)
         }
